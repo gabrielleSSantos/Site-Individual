73c09ddaefbc68e3f4b2eada8dd647571678b9f5
gabrielleSSantos <gabrielle.santos@sptech.school>
2022-05-10 21:59:12 -0300

    Pagina Login Pronta

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// let count = 1;
-// document.getElementById("radio1").ckecked = true ;
-
-// setInterval( function(){
-//     nextImage();
-// }, 2000)
-
-// function nextImage(){
-//     count++;
-//     if(count>4){
-//         count=1;
-//     }
-// document.getElementById("radio"+count).ckecked = true ;
-
-// }
 
 // ANIMAÇÃO DO TEXTO PAGINA INDEX
 var i = 0;
