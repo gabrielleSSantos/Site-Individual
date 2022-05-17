f06cc3ea79a7ff315913e1e9128e668c97165c26
gabrielleSSantos <gabrielle.santos@sptech.school>
2022-05-17 13:37:56 -0300

    API COMPLETA

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -2,30 +2,40 @@
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 /* para workbench - local - desenvolvimento */
-CREATE DATABASE acquatec;
+CREATE DATABASE GabiTrancas;
 
-USE acquatec;
+USE GabiTrancas;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+CREATE TABLE usuario(
+	idusuario INT PRIMARY KEY auto_increment,
+    nome VARCHAR(45),
+	email varchar(45),
+    senha varchar(20),
+    rua VARCHAR(45),
+    numero INT,
+    bairro VARCHAR(45)
 );
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
+ ALTER TABLE usuario add column FKindicacao int, add foreign key (FKindicacao) references usuario(idusuario);
+ 
+ CREATE TABLE Agendamentos(
+	idagendamentos INT primary KEY auto_increment,
+    dtHora DATETIME,
+    observavao VARCHAR(45),
+    TrancaEscolha VARCHAR(25),
+    valorTranca int,
+    FKusuario int, foreign key (FKusuario) references usuario(idusuario)
+ );
+ 
+ CREATE TABLE Avaliacoes(
+	idavaliacoes int primary KEY auto_increment,
+    descricao varchar(65),
+    nota int,
+    FKusuario int, foreign key (FKusuario) references usuario(idusuario)
+ );
+ 
+ 
+ insert into usuario values
+ (null, 'Gabrielle', 'gabrielle@gmail.com', 'gabi','rua doida', 20, 'inacio monteiro', null);
 
 /* altere esta tabela de acordo com o que está em INSERT de sua API do arduino */
 
